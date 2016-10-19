c838f432a609f85ba7f64eef20d8b02b9bd8b78e
Glenn Van Gastel <glenn.vangastel@icapps.com>
2016-10-19 10:44:23 +0200

    Changed syntax to ES6

--- fields/types/list/ListColumn.js
+++ fields/types/list/ListColumn.js
@@ -3,30 +3,26 @@ import ItemsTableCell from '../../components/ItemsTableCell';
 import ItemsTableValue from '../../components/ItemsTableValue';
 import { plural } from '../../../admin/client/utils/string';
 
-var ListColumn = React.createClass({
-	displayName: 'ListColumn',
-	propTypes: {
+export default class ListColumn extends React.Component {
+	displayName: 'ListColumn'
+	props: {
 		col: React.PropTypes.object,
 		data: React.PropTypes.object,
-	},
+	}
+
 	getValue () {
 		var value = this.props.data.fields[this.props.col.path];
-		if (Array.isArray(value)) {
-			return plural(value.length, '* Value', '* Values');
-		} else {
-			return '';
-		}
-	},
+		return Array.isArray(value) ? plural(value.length, '* Value', '* Values') : ''
+	}
+
 	render () {
-		const value = this.getValue();
+		const value = this.getValue()
 		return (
 			<ItemsTableCell>
-			<ItemsTableValue padded interior field={this.props.col.type}>
-		{value}
-	</ItemsTableValue>
-		</ItemsTableCell>
-	);
-	},
-});
-
-module.exports = ListColumn;
+				<ItemsTableValue padded interior field={this.props.col.type}>
+					{value}
+				</ItemsTableValue>
+			</ItemsTableCell>
+		)
+	}
+};
